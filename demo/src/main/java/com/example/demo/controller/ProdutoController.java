3a83b87dd05368bd2182d9b6d0b94da2c9c90b57
KelvinFV <kelvinfernandesfv@gmail.com>
2026-04-08 20:35:40 -0300

    Implementação inicial do banco de dados

---
+++ demo/src/main/java/com/example/demo/controller/ProdutoController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Produto;
+import com.example.demo.repository.ProdutoRepository;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+        
+        System.out.println("Produto salvo: " + produto);
+        
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+
+    }
+
+}
